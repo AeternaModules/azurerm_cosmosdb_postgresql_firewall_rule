77476dbadf2a4474ff443a2ec40bacc80cc14ddd
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:01:15 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "cosmosdb_postgresql_firewall_rules_id" {
+  description = "Map of id values across all cosmosdb_postgresql_firewall_rules, keyed the same as var.cosmosdb_postgresql_firewall_rules"
+  value       = { for k, v in azurerm_cosmosdb_postgresql_firewall_rule.cosmosdb_postgresql_firewall_rules : k => v.id }
+}
 output "cosmosdb_postgresql_firewall_rules_cluster_id" {
   description = "Map of cluster_id values across all cosmosdb_postgresql_firewall_rules, keyed the same as var.cosmosdb_postgresql_firewall_rules"
   value       = { for k, v in azurerm_cosmosdb_postgresql_firewall_rule.cosmosdb_postgresql_firewall_rules : k => v.cluster_id }
