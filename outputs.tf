6a03267df8be9fc4b97bc4e3b88984500452317b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:37 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "cosmosdb_postgresql_firewall_rules" {
-  description = "All cosmosdb_postgresql_firewall_rule resources"
-  value       = azurerm_cosmosdb_postgresql_firewall_rule.cosmosdb_postgresql_firewall_rules
-}
 output "cosmosdb_postgresql_firewall_rules_cluster_id" {
-  description = "List of cluster_id values across all cosmosdb_postgresql_firewall_rules"
-  value       = [for k, v in azurerm_cosmosdb_postgresql_firewall_rule.cosmosdb_postgresql_firewall_rules : v.cluster_id]
+  description = "Map of cluster_id values across all cosmosdb_postgresql_firewall_rules, keyed the same as var.cosmosdb_postgresql_firewall_rules"
+  value       = { for k, v in azurerm_cosmosdb_postgresql_firewall_rule.cosmosdb_postgresql_firewall_rules : k => v.cluster_id }
 }
 output "cosmosdb_postgresql_firewall_rules_end_ip_address" {
-  description = "List of end_ip_address values across all cosmosdb_postgresql_firewall_rules"
-  value       = [for k, v in azurerm_cosmosdb_postgresql_firewall_rule.cosmosdb_postgresql_firewall_rules : v.end_ip_address]
+  description = "Map of end_ip_address values across all cosmosdb_postgresql_firewall_rules, keyed the same as var.cosmosdb_postgresql_firewall_rules"
+  value       = { for k, v in azurerm_cosmosdb_postgresql_firewall_rule.cosmosdb_postgresql_firewall_rules : k => v.end_ip_address }
 }
 output "cosmosdb_postgresql_firewall_rules_name" {
-  description = "List of name values across all cosmosdb_postgresql_firewall_rules"
-  value       = [for k, v in azurerm_cosmosdb_postgresql_firewall_rule.cosmosdb_postgresql_firewall_rules : v.name]
+  description = "Map of name values across all cosmosdb_postgresql_firewall_rules, keyed the same as var.cosmosdb_postgresql_firewall_rules"
+  value       = { for k, v in azurerm_cosmosdb_postgresql_firewall_rule.cosmosdb_postgresql_firewall_rules : k => v.name }
 }
 output "cosmosdb_postgresql_firewall_rules_start_ip_address" {
-  description = "List of start_ip_address values across all cosmosdb_postgresql_firewall_rules"
-  value       = [for k, v in azurerm_cosmosdb_postgresql_firewall_rule.cosmosdb_postgresql_firewall_rules : v.start_ip_address]
+  description = "Map of start_ip_address values across all cosmosdb_postgresql_firewall_rules, keyed the same as var.cosmosdb_postgresql_firewall_rules"
+  value       = { for k, v in azurerm_cosmosdb_postgresql_firewall_rule.cosmosdb_postgresql_firewall_rules : k => v.start_ip_address }
 }
 
